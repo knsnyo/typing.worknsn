497e63ee54bf9716698c01f5d04bd64710cc87db
knsnyo <isc963@naver.com>
2023-05-12 16:03:07 +0900

    20230512 update middleware auth

--- server/src/middleware/Auth.ts
+++ server/src/middleware/Auth.ts
@@ -1,22 +1,34 @@
 import tokenService from '@/services/tokenService';
+import errorCatch from '@/utils/errCatch';
 import { Request, Response, NextFunction } from 'express';
 import { JwtPayload } from 'jsonwebtoken';
 
-const Auth = (req: Request, res: Response, next: NextFunction): void => {
-  const accessToken: string = req.body.accessToken ?? req.get('accessToken');
-  const refreshToken: string = req.body.refreshToken ?? req.get('refreshToken');
+const checkAccess = (req: Request, res: Response, next: NextFunction): void => {
+  const accessToken: string = req.get('accessToken') ?? '';
   const { idx, verify }: { idx: number; verify: boolean } = tokenService.verifyAccess(accessToken);
   if (verify) {
     req.body.idx = idx;
-    return next();
+    return next('route');
   }
-  const data: JwtPayload = tokenService.verifyRefresh(refreshToken);
-  const newAccessToken = tokenService.createAccess(data.idx, data.id);
-  const newRefreshToken = tokenService.createRefresh(data.idx, data.id);
-  req.body.idx = data.idx;
-  req.body.accessToken = newAccessToken;
-  req.body.refreshToken = newRefreshToken;
   return next();
 };
 
-export default Auth;
+const checkRefresh = (req: Request, res: Response, next: NextFunction): void | Response => {
+  const refreshToken: string = req.get('refreshToken') ?? '';
+  try {
+    const data: JwtPayload = tokenService.verifyRefresh(refreshToken);
+    const newAccessToken = tokenService.createAccess(data.idx, data.id);
+    const newRefreshToken = tokenService.createRefresh(data.idx, data.id);
+    req.body.idx = data.idx;
+    req.body.accessToken = newAccessToken;
+    req.body.refreshToken = newRefreshToken;
+    return next();
+  } catch (err: unknown) {
+    return errorCatch(err, res);
+  }
+};
+
+export const Auth = {
+  checkAccess,
+  checkRefresh,
+};
--- server/src/routes/infoRouter.ts
+++ server/src/routes/infoRouter.ts
@@ -1,11 +1,11 @@
 import chulseokController from '@/controllers/chulseokController';
 import recordController from '@/controllers/recordController';
-import Auth from '@/middleware/Auth';
+import { Auth } from '@/middleware/Auth';
 import { Router } from 'express';
 
 const infoRouter: Router = Router();
 
-infoRouter.use(Auth);
+infoRouter.route('*').all(Auth.checkAccess, Auth.checkRefresh);
 infoRouter.route('/info/records').get(recordController.getInfo).post(recordController.insert);
 infoRouter.route('/info/chulseoks').get(chulseokController.getList);
 // infoRouter.route('/avg-max').get();
